64213d11c7d1207246c11dc48cdd49d9e01f08f3
jamiekariuki <jamiekariuki18@gmail.com>
2025-08-27 22:11:11 +0300

    fix/configured ecr tokens

--- infrastructure/terraform/backend.tf
+++ infrastructure/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "XXXXXXXXXXXX-bucket-state-file-karpenter"
-    region = "ap-southeast-2"
+    region = "us-east-1"
     key    = "karpenter.tfstate"
   }
 }
