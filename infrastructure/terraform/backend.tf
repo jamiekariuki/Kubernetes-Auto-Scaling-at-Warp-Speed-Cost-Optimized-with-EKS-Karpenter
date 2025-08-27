f4cd445d12cf37f96a4ae1f1715eb007d21dc44a
jamiekariuki <jamiekariuki18@gmail.com>
2025-08-28 01:40:03 +0300

    fix/upgraded terrafrom version

--- infrastructure/terraform/backend.tf
+++ infrastructure/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "XXXXXXXXXXXX-bucket-state-file-karpenter"
+    bucket = "850502433430-bucket-state-file-karpenter"
     region = "us-east-1"
     key    = "karpenter.tfstate"
   }
